8f2b31a55f443e15a935f54a91f15a126ebe0a70
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2021-08-20 16:34:09 -0300

    Update Dom_conheCdores_view.c

--- Dom_conheCdores_view.c
+++ Dom_conheCdores_view.c
@@ -1,11 +1,12 @@
 //view
-
 //#include<stdio.h>
 
 
 int menu();
 void apresentaMesa(pecas Tpecas[28], mesa pecaMesa[28]);
-int Maiorpeca(pecas Tepcas[28]);
+void Jogada(pecas Tpecas[28], mesa pecaMesa[28]);
+int PrimeiraJogada(pecas Tepcas[28], mesa pecaMesa[28]);
+void mostraPrimeiroJogador(pecas Tpecas[28], mesa pecaMesa[28]);
 
 int menu()
 {
@@ -25,35 +26,51 @@ int menu()
 }
 
 
-
-
 void apresentaMesa(pecas Tpecas[28], mesa pecaMesa[28])
 {
-	int y;
-	y=Maiorpeca(Tpecas);
-	printf("%d", y);
+	int y, i;
+	
+	printf("\n-------------\n");
+	printf("MESA: [ %d | %d ]\n", pecaMesa[0].pecaJogada[0], pecaMesa[0].pecaJogada[1]);
+	printf("-------------\n");
 	
 }
-int Maiorpeca(pecas Tpecas[28])
+
+void Jogada(pecas Tpecas[28], mesa pecaMesa[28])
 {
-	int i, x=0, maior=0;
-	
-	for(i=0;i<28;i++)
+	int i, x;
+	x=PrimeiraJogada(Tpecas, pecaMesa);
+	if(x==1)
 	{
+		printf("Jogador 1 pecas:");
+		for(i=0;i<28;i++)
+		{
 		if(Tpecas[i].local=='j')
-			printf("%d- (%d | %d ) ", i,  Tpecas[i].peca[0], Tpecas[i].peca[1]);
+			printf(" %d- (%d | %d ) ", i+1,  Tpecas[i].peca[0], Tpecas[i].peca[1]);
+		}
 	}
-	printf("\n");
-	for(i=0;i<28;i++)
+	else
 	{
+		printf("Jogador 2 pecas:");
+		for(i=0;i<28;i++)
+		{
 		if(Tpecas[i].local=='a')
-			printf("%d-  (%d | %d ) ", i, Tpecas[i].peca[0], Tpecas[i].peca[1]);
+			printf(" %d- (%d | %d ) ", i+1,  Tpecas[i].peca[0], Tpecas[i].peca[1]);
+		}
 	}
-	
 	printf("\n");
+	
+	
+}
+
+int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1 peca foi do jogador 1 ou 2 
+{	
+														
+	int i, x=0, maior=0, y;
+
 	for(i=0;i<28;i++)
 	{
-		if(Tpecas[i].local=='j'||Tpecas[i].local=='a')
+		if(Tpecas[i].local!='d')
 		{
 			if(Tpecas[i].peca[0]==Tpecas[i].peca[1])
 			{
@@ -63,8 +80,45 @@ int Maiorpeca(pecas Tpecas[28])
 					x=i;
 				}
 			}
-			
 		}
 	}
-	return x;
+	if(maior==0)
+	{
+		for(i=0;i<28;i++)
+		{
+			if(Tpecas[i].peca[0]+Tpecas[i].peca[1]>maior)
+			{
+				maior=Tpecas[i].peca[0]+Tpecas[i].peca[1];
+				x=i;
+			}
+		}
+	}
+	if(x<6)
+	{
+		y=1;
+	}
+	else
+	{
+		y=2;
+	}
+	pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
+	pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
+	Tpecas[x].local='m';
+	
+	return y;
+}
+
+void mostraPrimeiroJogador(pecas Tpecas[28], mesa pecaMesa[28])
+{
+	int y;
+	y=PrimeiraJogada(Tpecas, pecaMesa);
+	if(y==1)
+	{
+		printf("\na primeira jogada foi jogador 1, pois tem a maior peca\n");
+	}
+	else
+	{
+		printf("\na primeira jogada foi jogador 2, pois tem a maior peca\n");
+	}
+	printf("\n");
 }
