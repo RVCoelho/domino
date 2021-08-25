10d1fb3afda0a0ccd02a0eca3392949699e222df
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2021-08-25 12:43:28 -0300

    Update Dom_conheCdores_view.c

--- Dom_conheCdores_view.c
+++ Dom_conheCdores_view.c
@@ -10,7 +10,6 @@ void apresentaMesa(pecas Tpecas[28], mesa pecaMesa[28]);
 
 void pecasJogador(pecas Tpecas[28], mesa pecaMesa[28]);
 
-
 void mostraPrimeiroJogador(pecas Tpecas[28], mesa pecaMesa[28]);
 
 char opcoesPjogar(mesa pecaMesa[28]);
@@ -75,8 +74,6 @@ void pecasJogador(pecas Tpecas[28], mesa pecaMesa[28])
 	printf("\n------------\n");
 }
 
-//funcao para definir a primeira peca a ser jogada
-
 //funcao apenas para mostrar de quem foi a primeira peca a ser jogada
 void mostraPrimeiroJogador(pecas Tpecas[28], mesa pecaMesa[28])
 {
@@ -97,9 +94,9 @@ char opcoesPjogar( mesa pecaMesa[28])
 {
 	char c;
 	
-	printf(" j- jogar(possiveis: %d ou %d)\n", pecaMesa[0].ladoE, pecaMesa[0].ladoD);
+	printf(" j- jogar(possiveis: %d ou %d)\n", extremidadeE, extremidadeD);
 	printf(" c- Comprar\n");
-	printf(" p- Passar a vez\n");
+	printf(" p- Passar a vez, se possivel\n");
 	printf(" s- Sair(interromper o jogo)\n");
 	printf(" Opcao:");
 	scanf("%s", &c);
@@ -110,7 +107,7 @@ char opcoesPjogar( mesa pecaMesa[28])
 
 void Jogadas(pecas Tpecas[28], mesa pecaMesa[28])
 {
-	char c;
+	char c, a='o';
 	int x=0, y;
 		
 	c=opcoesPjogar(pecaMesa);
@@ -125,14 +122,27 @@ void Jogadas(pecas Tpecas[28], mesa pecaMesa[28])
 				scanf("%d", &y);
 				y=y-1;
 				x=verifica(y, Tpecas, pecaMesa);
-				printf("%d\n", x);
-				if(x==2)
+				if(x==2){
 					printf("\nA peca escolhida nao pode ser jogada.\n");
+				}
+				if(Tpecas[y].peca[0]== extremidadeD || extremidadeE)
+				{
+					printf("escolha o lado da mesa para jogar(d=direito, e=esquerdo)");
+					scanf("%c", &a);
+				}
+				else if(Tpecas[y].peca[1]== extremidadeD || extremidadeE)
+				{
+					printf("escolha o lado da mesa para jogar(d=direito, e=esquerdo)");
+					scanf("%c", &a);
+				}
+
 			}
-			adicionaNaMesa(y, Tpecas, pecaMesa);
+			adicionaNaMesa(y, a, Tpecas, pecaMesa);
 			apresentaMesa(Tpecas, pecaMesa);
 			break;
+
 		case 'c':
+
 			compraPeca(Tpecas, pecaMesa);
 			pecasJogador(Tpecas, pecaMesa);
 			break;
