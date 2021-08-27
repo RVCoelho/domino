15a346e31eb9bfa54a54c5d5fac9d0311ecdc354
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2021-08-27 16:44:02 -0300

    Update Dom_conheCdores_control.c

--- Dom_conheCdores_control.c
+++ Dom_conheCdores_control.c
@@ -10,10 +10,12 @@ void distribui(pecas Tpecas[28]);
 void compraPeca(pecas Tpecas[28], mesa pecaMesa[28]);
 int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]);
 void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28]);
+int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28]);
+int AcabaJogo(int y, pecas Tpecas[28]);
 
 void Domino(pecas Tpecas[28], mesa pecaMesa[28])
 {
-	int o, y;
+	int o, y, h=0;
 	o=menu();
 	switch(o)
 	{
@@ -24,14 +26,23 @@ void Domino(pecas Tpecas[28], mesa pecaMesa[28])
 			PrimeiraJogada(Tpecas, pecaMesa);
 			mostraPrimeiroJogador(Tpecas, pecaMesa);
 			apresentaMesa(Tpecas, pecaMesa);
-			pecasJogador(Tpecas, pecaMesa);
-			Jogadas(Tpecas,pecaMesa);
+			while(h!=2)
+			{
+				pecasJogador(Tpecas, pecaMesa);
+				Jogadas(Tpecas,pecaMesa);
+				h=AcabaJogo(qualJogador, Tpecas);
+			}
+			printf("\nOBRIGADO POR JOGAR\n");
 			break;
 			
 		case 2:
 			printf("");	
 			break;
 				
+		case 4:
+			regras();
+
+			break;
 		case 5:
 			printf("OBRIGADO POR JOGAR");
 			break;
@@ -100,33 +111,6 @@ void distribui(pecas Tpecas[28])
 	}
 }
 
-void compraPeca(pecas Tpecas[28], mesa pecaMesa[28])
-{
-	int i, x, k, cont;
-	x=qualJogador;
-	if(x%2!=0)
-	{
-		for(i=0; i<28; i++)
-		{
-			if(Tpecas[i].local=='d')
-			{
-				Tpecas[i].local='j';
-				break;
-			}
-		}
-	}
-	else
-	{
-		for(i=0; i<28; i++)
-		{
-			if(Tpecas[i].local=='d')
-			{
-				Tpecas[i].local='a';
-				break;
-			}
-		}
-	}
-}
 
 
 int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28])
@@ -200,8 +184,8 @@ int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1
 	pecaMesa[0].rodadas=1;
 
 	//define os lados da peca jogada
-	extremidadeE=Tpecas[x].peca[0];
-	extremidadeD=Tpecas[x].peca[1];
+	extremidadeE=pecaMesa[0].pecaJogada[0];
+	extremidadeD=pecaMesa[0].pecaJogada[1];
 
 	//retorna quem foi o primeiro jogador
 	return y;
@@ -209,74 +193,160 @@ int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1
 
 void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28])
 {
-	int y, i;
+	int y, i, k;
 	y=pecaMesa->rodadas;
 	if(a=='o')
 	{
-		for(i=0; i<=y; i++)
+		if(Tpecas[x].peca[0]==extremidadeE||Tpecas[x].peca[1]==extremidadeE)
 		{
-			pecaMesa[i+1].pecaJogada[0]=pecaMesa[i].pecaJogada[0];
-			pecaMesa[i+1].pecaJogada[1]=pecaMesa[i].pecaJogada[1];
+			for(i=y; i>0; i--)//joga a mesa para a direita para incerir uma peca na esquerda
+			{
+				pecaMesa[i].pecaJogada[0]=pecaMesa[i-1].pecaJogada[0];
+				pecaMesa[i].pecaJogada[1]=pecaMesa[i-1].pecaJogada[1];
+			}
+			if(Tpecas[x].peca[1]==extremidadeE)//caso a peca esteja virada do lado certo
+			{
+				pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
+				pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
+				extremidadeE=Tpecas[x].peca[0];
+				extremidadeD=pecaMesa[y].pecaJogada[1];//
+			}
+			else//caso a peca esteja virada do lado errado
+			{
+				pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[0];
+				pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[1];
+				extremidadeE=Tpecas[x].peca[1];
+				extremidadeD=pecaMesa[y].pecaJogada[1];//
+			}
+			Tpecas[x].local='m';
+			pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
+			qualJogador++;
 		}
-		if(Tpecas[x].peca[1]==extremidadeE)
+		else if(Tpecas[x].peca[0]==extremidadeD||Tpecas[x].peca[1]==extremidadeD)
 		{
-			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
-			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
-			extremidadeE=Tpecas[x].peca[0];
-			extremidadeD=pecaMesa[i].pecaJogada[1];
+			pecaMesa[y].pecaJogada[1]=Tpecas[x].peca[1];
+			pecaMesa[y].pecaJogada[0]=Tpecas[x].peca[0];
+
+			if(extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
+			{
+				k=pecaMesa[y].pecaJogada[0];
+				pecaMesa[y].pecaJogada[0]=pecaMesa[y].pecaJogada[1];
+				pecaMesa[y].pecaJogada[1]=k;
+			}
+			Tpecas[x].local='m';
+			extremidadeD=pecaMesa[y].pecaJogada[1];
+			pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
+			qualJogador++;
 		}
-		else
-		{
-			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[0];
-			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[1];
-			extremidadeE=Tpecas[x].peca[1];
-			extremidadeD=pecaMesa[i].pecaJogada[1];
-		}
-		printf("\n%d - %d\n", extremidadeE, extremidadeD);
-		Tpecas[x].local='m';
-		pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
-		qualJogador++;
+
+
 	}
 	else if(a=='e')
 	{
-		for(i=0; i<=y; i++)
+		for(i=y; i>0; i--)//joga a mesa para a direita para incerir uma peca na esquerda
 		{
-			pecaMesa[i+1].pecaJogada[0]=pecaMesa[i].pecaJogada[0];
-			pecaMesa[i+1].pecaJogada[1]=pecaMesa[i].pecaJogada[1];
+			pecaMesa[i].pecaJogada[0]=pecaMesa[i-1].pecaJogada[0];
+			pecaMesa[i].pecaJogada[1]=pecaMesa[i-1].pecaJogada[1];
 		}
 		if(Tpecas[x].peca[1]==extremidadeE)
 		{
 			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
 			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
 			extremidadeE=Tpecas[x].peca[0];
-			extremidadeD=pecaMesa[i].pecaJogada[1];
+			extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
 		}
-		else
+		else if(Tpecas[x].peca[1]!=extremidadeE)
 		{
 			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[0];
 			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[1];
-			extremidadeE=Tpecas[x].peca[1];
-			extremidadeD=pecaMesa[i].pecaJogada[1];
+			extremidadeE=pecaMesa[0].pecaJogada[0];
+			extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
 		}
-		printf("\n%d - %d\n", extremidadeE, extremidadeD);
 		Tpecas[x].local='m';
 		pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
 		qualJogador++;
 	}
 	else if(a=='d')
 	{
-		pecaMesa[y+1].pecaJogada[0]=Tpecas[x].peca[0];
-		pecaMesa[y+1].pecaJogada[1]=Tpecas[x].peca[1];
+		pecaMesa[y].pecaJogada[0]=Tpecas[x].peca[0];
+		pecaMesa[y].pecaJogada[1]=Tpecas[x].peca[1];
 
-		if(extremidadeD!=pecaMesa[y+1].pecaJogada[0])
+		if(extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
 		{
-			pecaMesa[y+1].pecaJogada[0]=pecaMesa[y+1].pecaJogada[1];
-			pecaMesa[y+1].pecaJogada[1]=pecaMesa[y+1].pecaJogada[0];
+			k=pecaMesa[y].pecaJogada[0];
+			pecaMesa[y].pecaJogada[0]=pecaMesa[y].pecaJogada[1];
+			pecaMesa[y].pecaJogada[1]=k;
 		}
-		extremidadeD=pecaMesa[y+1].pecaJogada[1];
+		extremidadeD=pecaMesa[y].pecaJogada[1];
 		Tpecas[x].local='m';
 		pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
 		qualJogador++;
 
 	}	
 }
+
+int AcabaJogo( int y, pecas Tpecas[28])
+{
+	int cont=0, i;	
+	y=y-1;
+	if(y%2!=0)
+	{
+		for(i=0;i<28;i++)
+		{
+			if(Tpecas[i].local=='j')
+			{
+				cont++;
+			}		
+		}
+	}
+	else
+	{
+		for(i=0;i<28;i++)
+		{
+			if(Tpecas[i].local=='a')
+			{
+				cont++;
+			}
+		}
+	}
+
+	if(cont==0)
+	{
+		printf("\nVOCE GANHOU O JOGO, PARABENS\n");
+		return 2;//para encerrar o jogo
+	}
+	else
+	{
+		return 1;//para continuar o jogo
+	}
+}	
+
+
+void compraPeca(pecas Tpecas[28], mesa pecaMesa[28])
+{
+	int i, x, k, cont;
+	x=qualJogador;
+	if(x%2!=0)
+	{
+		for(i=0; i<28; i++)
+		{
+			if(Tpecas[i].local=='d')
+			{
+				Tpecas[i].local='j';
+				break;
+			}
+		}
+	}
+	else
+	{
+		for(i=0; i<28; i++)
+		{
+			if(Tpecas[i].local=='d')
+			{
+				Tpecas[i].local='a';
+				break;
+			}
+		}
+	}
+}
+	
