c1333b5be645010a58de6a0733b5439b447c6243
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2021-09-12 16:57:54 -0300

    Update Dom_conheCdores_control.c

--- Dom_conheCdores_control.c
+++ Dom_conheCdores_control.c
@@ -3,19 +3,30 @@
 
 
 
-void Domino(pecas Tpecas[28], mesa pecaMesa[28]);
+void Domino(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]);
+
 void geraPeca(pecas Tpecas[28]);
+
 void embaralha(pecas Tpecas[28]);
+
 void distribui(pecas Tpecas[28]);
-void compraPeca(pecas Tpecas[28], mesa pecaMesa[28]);
-int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]);
-void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28]);
-int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28]);
+
+void compraPeca(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]);
+
+int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]);
+
+void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]);
+
+int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]);
+
 int AcabaJogo(int y, pecas Tpecas[28]);
-int Empate(pecas Tpecas[28]);
+
+int Empate(pecas Tpecas[28], Variaveis VA[1]);
+
 void GravaJogo(pecas Tpecas[28], mesa pecaMesa[28]);
 
-void Domino(pecas Tpecas[28], mesa pecaMesa[28])
+
+void Domino(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1])
 {
 	int o, y, h=0;
 	while (o!=5)
@@ -27,23 +38,28 @@ void Domino(pecas Tpecas[28], mesa pecaMesa[28])
 				geraPeca(Tpecas);
 				embaralha(Tpecas);
 				distribui(Tpecas);
-				PrimeiraJogada(Tpecas, pecaMesa);
-				mostraPrimeiroJogador(Tpecas, pecaMesa);
-				apresentaMesa(Tpecas, pecaMesa);
+				//PrimeiraJogada(Tpecas, pecaMesa,VA);
+				mostraPrimeiroJogador(Tpecas, pecaMesa, VA);
+				apresentaMesa(Tpecas, pecaMesa, VA);
 				while(h!=2)
 				{
-					pecasJogador(Tpecas, pecaMesa);
-					h=Empate(Tpecas);
+					pecasJogador(Tpecas, pecaMesa, VA);
+					h=Empate(Tpecas, VA);
 					if(h==2)
 						break;
-					Jogadas(Tpecas,pecaMesa);
-					h=AcabaJogo(qualJogador, Tpecas);
+					Jogadas(Tpecas,pecaMesa, VA);
+					h=AcabaJogo(VA[0].qualJogador, Tpecas);
 				}
 				printf("\nOBRIGADO POR JOGAR\n");
 				break;
 
 			case 2:
-				printf("");	
+				geraPeca(Tpecas);
+				embaralha(Tpecas);
+				distribui(Tpecas);
+				mostraPrimeiroJogadorVScomputador(Tpecas, pecaMesa, VA);
+				apresentaMesa(Tpecas, pecaMesa, VA);
+				pecasJogadorVScomputador(Tpecas, pecaMesa, VA);
 				break;
 			
 			case 3:	
@@ -124,15 +140,15 @@ void distribui(pecas Tpecas[28])
 
 
 
-int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28])//funcao que verifica se a peca escolhida pode ser jogada na mesa
+int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1])//funcao que verifica se a peca escolhida pode ser jogada na mesa
 {
 	int x;
 	
-	if(Tpecas[y].peca[0]== extremidadeD || Tpecas[y].peca[0]== extremidadeE)
+	if(Tpecas[y].peca[0]==VA[0].extremidadeD || Tpecas[y].peca[0]==VA[0].extremidadeE)
 	{
 		x=1; //1 = peca que pode ser jogada na mesa
 	}
-	else if(Tpecas[y].peca[1]== extremidadeD || Tpecas[y].peca[1]== extremidadeE)
+	else if(Tpecas[y].peca[1]==VA[0].extremidadeD || Tpecas[y].peca[1]==VA[0].extremidadeE)
 	{
 		x=1;
 	}
@@ -144,7 +160,7 @@ int verifica(int y, pecas Tpecas[28], mesa pecaMesa[28])//funcao que verifica se
 	return x;
 }
 
-int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1 peca foi do jogador 1 ou 2 
+int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1]) //retorna o y=1 ou y=2,1 peca foi do jogador 1 ou 2 
 {	//funcao que realiza a primeira jogada do jogo, pois a primeira jogada é automatica
 														
 	int i, x=0, maior=0, y;
@@ -177,12 +193,12 @@ int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1
 	if(x<7)
 	{
 		y=1;
-		qualJogador=2; //variavel para saber qual é o jogador que esta na vez
+		VA[0].qualJogador=2; //variavel para saber qual é o jogador que esta na vez
 	}
 	else
 	{
 		y=2;
-		qualJogador=1;
+		VA[0].qualJogador=1;
 	}
 	//passa a peca da mao do jogador para a mesa
 	pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
@@ -192,62 +208,62 @@ int PrimeiraJogada(pecas Tpecas[28], mesa pecaMesa[28]) //retorna o y=1 ou y=2,1
 	Tpecas[x].local='m';
 
 	//aumenta o numero de rodadas para 1
-	pecaMesa[0].rodadas=1;
+	VA[0].rodadas=1;
 
 	//define os lados da peca jogada
-	extremidadeE=pecaMesa[0].pecaJogada[0];
-	extremidadeD=pecaMesa[0].pecaJogada[1];
+	VA[0].extremidadeE=pecaMesa[0].pecaJogada[0];
+	VA[0].extremidadeD=pecaMesa[0].pecaJogada[1];
 
 	//retorna quem foi o primeiro jogador
 	return y;
 }
 
-void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28])
+void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1])
 {
 	int y, i, k;
-	y=pecaMesa->rodadas;
+	y=VA[0].rodadas;
 	if(a=='o')
 	{
-		if(Tpecas[x].peca[0]==extremidadeE||Tpecas[x].peca[1]==extremidadeE)
+		if(Tpecas[x].peca[0]==VA[0].extremidadeE||Tpecas[x].peca[1]==VA[0].extremidadeE)
 		{
 			for(i=y; i>0; i--)//joga a mesa para a direita para incerir uma peca na esquerda
 			{
 				pecaMesa[i].pecaJogada[0]=pecaMesa[i-1].pecaJogada[0];
 				pecaMesa[i].pecaJogada[1]=pecaMesa[i-1].pecaJogada[1];
 			}
-			if(Tpecas[x].peca[1]==extremidadeE)//caso a peca esteja virada do lado certo
+			if(Tpecas[x].peca[1]==VA[0].extremidadeE)//caso a peca esteja virada do lado certo
 			{
 				pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
 				pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
-				extremidadeE=Tpecas[x].peca[0];
-				extremidadeD=pecaMesa[y].pecaJogada[1];
+				VA[0].extremidadeE=Tpecas[x].peca[0];
+				VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];
 			}
 			else//caso a peca esteja virada do lado errado
 			{
 				pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[0];
 				pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[1];
-				extremidadeE=Tpecas[x].peca[1];
-				extremidadeD=pecaMesa[y].pecaJogada[1];//
+				VA[0].extremidadeE=Tpecas[x].peca[1];
+				VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];//
 			}
 			Tpecas[x].local='m';//muda o local da peca para a mesa
-			pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;//aumenta o numero de rodadas 
-			qualJogador++;//passa a vez para o proximo jogador
+			VA[0].rodadas=VA[0].rodadas+1;//aumenta o numero de rodadas 
+			VA[0].qualJogador++;//passa a vez para o proximo jogador
 		}
-		else if(Tpecas[x].peca[0]==extremidadeD||Tpecas[x].peca[1]==extremidadeD)//caso a peca tenha q ir para a extremidade direita da mesa
+		else if(Tpecas[x].peca[0]==VA[0].extremidadeD||Tpecas[x].peca[1]==VA[0].extremidadeD)//caso a peca tenha q ir para a extremidade direita da mesa
 		{
 			pecaMesa[y].pecaJogada[1]=Tpecas[x].peca[1];//so adicionando uma peca na extremidade direita
 			pecaMesa[y].pecaJogada[0]=Tpecas[x].peca[0];//so adicionando uma peca na extremidade direita
 
-			if(extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
+			if(VA[0].extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
 			{
 				k=pecaMesa[y].pecaJogada[0];
 				pecaMesa[y].pecaJogada[0]=pecaMesa[y].pecaJogada[1];
 				pecaMesa[y].pecaJogada[1]=k;
 			}
 			Tpecas[x].local='m';
-			extremidadeD=pecaMesa[y].pecaJogada[1];//muda a extremidade da mesa para a nova peca
-			pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;//aumenta o numero de rodadas
-			qualJogador++;//passa a vez para o proximo jogador
+			VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];//muda a extremidade da mesa para a nova peca
+			VA[0].rodadas=VA[0].rodadas+1;//aumenta o numero de rodadas
+			VA[0].qualJogador++;//passa a vez para o proximo jogador
 		}
 
 
@@ -259,39 +275,39 @@ void adicionaNaMesa(int x, char a, pecas Tpecas[28], mesa pecaMesa[28])
 			pecaMesa[i].pecaJogada[0]=pecaMesa[i-1].pecaJogada[0];
 			pecaMesa[i].pecaJogada[1]=pecaMesa[i-1].pecaJogada[1];
 		}
-		if(Tpecas[x].peca[1]==extremidadeE)
+		if(Tpecas[x].peca[1]==VA[0].extremidadeE)
 		{
 			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[0];
 			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[1];
-			extremidadeE=Tpecas[x].peca[0];
-			extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
+			VA[0].extremidadeE=Tpecas[x].peca[0];
+			VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
 		}
-		else if(Tpecas[x].peca[1]!=extremidadeE)
+		else if(Tpecas[x].peca[1]!=VA[0].extremidadeE)
 		{
 			pecaMesa[0].pecaJogada[1]=Tpecas[x].peca[0];
 			pecaMesa[0].pecaJogada[0]=Tpecas[x].peca[1];
-			extremidadeE=pecaMesa[0].pecaJogada[0];
-			extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
+			VA[0].extremidadeE=pecaMesa[0].pecaJogada[0];
+			VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];//nao sei
 		}
 		Tpecas[x].local='m';
-		pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
-		qualJogador++;
+		VA[0].rodadas=VA[0].rodadas+1;
+		VA[0].qualJogador++;
 	}
 	else if(a=='d')//caso o jogador escolha jogar no lado direito
 	{
 		pecaMesa[y].pecaJogada[0]=Tpecas[x].peca[0];
 		pecaMesa[y].pecaJogada[1]=Tpecas[x].peca[1];
 
-		if(extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
+		if(VA[0].extremidadeD!=pecaMesa[y].pecaJogada[0])//caso a peca esteja virada do lado errado
 		{
 			k=pecaMesa[y].pecaJogada[0];
 			pecaMesa[y].pecaJogada[0]=pecaMesa[y].pecaJogada[1];
 			pecaMesa[y].pecaJogada[1]=k;
 		}
-		extremidadeD=pecaMesa[y].pecaJogada[1];
+		VA[0].extremidadeD=pecaMesa[y].pecaJogada[1];
 		Tpecas[x].local='m';
-		pecaMesa[0].rodadas=pecaMesa[0].rodadas+1;
-		qualJogador++;
+		VA[0].rodadas=VA[0].rodadas+1;
+		VA[0].qualJogador++;
 
 	}	
 }
@@ -333,10 +349,10 @@ int AcabaJogo( int y, pecas Tpecas[28])//funcao que verifica se resta alguma pec
 }	
 
 
-void compraPeca(pecas Tpecas[28], mesa pecaMesa[28])//funcao que compra pecas da mesa e insere na mao do jogador q a comprou
+void compraPeca(pecas Tpecas[28], mesa pecaMesa[28], Variaveis VA[1])//funcao que compra pecas da mesa e insere na mao do jogador q a comprou
 {
 	int i, x, k, cont;
-	x=qualJogador;
+	x=VA[0].qualJogador;
 	if(x%2!=0)
 	{
 		for(i=0; i<28; i++)
@@ -365,7 +381,7 @@ void compraPeca(pecas Tpecas[28], mesa pecaMesa[28])//funcao que compra pecas da
 	}
 }
 
-int Empate(pecas Tpecas[28])
+int Empate(pecas Tpecas[28], Variaveis VA[1])
 {
 	int i, cont=0, x=0, result1=0, result2=0;;
 
@@ -382,11 +398,11 @@ int Empate(pecas Tpecas[28])
 		{
 			if(Tpecas[i].local=='j')
 			{
-				if(Tpecas[i].peca[0]==extremidadeD || Tpecas[i].peca[0]==extremidadeE)
+				if(Tpecas[i].peca[0]==VA[0].extremidadeD || Tpecas[i].peca[0]==VA[0].extremidadeE)
 				{
 					x++;
 				}
-				else if(Tpecas[i].peca[1]==extremidadeD || Tpecas[i].peca[1]==extremidadeE)
+				else if(Tpecas[i].peca[1]==VA[0].extremidadeD || Tpecas[i].peca[1]==VA[0].extremidadeE)
 				{
 					x++;
 				}
@@ -396,11 +412,11 @@ int Empate(pecas Tpecas[28])
 		{
 			if(Tpecas[i].local=='a')
 			{
-				if(Tpecas[i].peca[0]==extremidadeD || Tpecas[i].peca[0]==extremidadeE)
+				if(Tpecas[i].peca[0]==VA[0].extremidadeD || Tpecas[i].peca[0]==VA[0].extremidadeE)
 				{
 					x++;
 				}
-				else if(Tpecas[i].peca[1]==extremidadeD || Tpecas[i].peca[1]==extremidadeE)
+				else if(Tpecas[i].peca[1]==VA[0].extremidadeD || Tpecas[i].peca[1]==VA[0].extremidadeE)
 				{
 					x++;
 				}
@@ -445,10 +461,10 @@ int Empate(pecas Tpecas[28])
 	}	
 }
 
-void GravaJogo(pecas Tpecas[28], mesa pecaMesa[28])
+/*void GravaJogo(pecas Tpecas[28], mesa pecaMesa[28])
 {
 	int i, y;
-	y=pecaMesa->rodadas;
+	y=rodadas;
 	if(ap=fopen("PecasJ", "w")==NULL)
 	{
 		printf("O arquivo PecasJ nao pode ser aberto para gravacao");
@@ -472,5 +488,6 @@ void GravaJogo(pecas Tpecas[28], mesa pecaMesa[28])
 			printf("erro na gravacao do arquivo pecasM");
 		}
 	}
-}	
+}*/
+
 
