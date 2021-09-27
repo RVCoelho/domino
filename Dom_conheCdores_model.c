f42191fefeadf933f4933314d7fb3f1cec820bc7
victor padilha <82392118+victorpadilha2001@users.noreply.github.com>
2021-09-27 14:42:52 -0300

    Update Dom_conheCdores_model.c

--- Dom_conheCdores_model.c
+++ Dom_conheCdores_model.c
@@ -1,34 +1,32 @@
 //arquivo model
 
-//arquivo model
 
-FILE *ap;
-FILE *am;
-FILE *av;
-
-typedef struct MaoJogador
+typedef struct MaoJogador		// estrutura que guarda todas as pecas do jogo
 {
-	int peca[2];
-	char local;
+	int peca[2];				//variavel para guardar os dois valores de uma peca
+	char local;					//para guardar o local onde a peca se encontra, ex j1, j2, mesa ou pote de compra
 	
-}pecas;
+} pecas;
 
-typedef struct tabuleiro
+typedef struct tabuleiro		//estrutura para guardar as  pecas que sao jogadas na mesa
 {
-	int ladoE;
-	int ladoD;
-	int pecaJogada[2];
-}mesa;
+	int pecaJogada[2];			//variavel para guardar os dois valores de uma peca jogada na mesa
 
-typedef struct Tvariaveis
+} mesa;
+
+typedef struct Tvariaveis		//estrutura para guardar as variaveis usadas na logica do jogo
 {
-	int qualJogador;
-	int extremidadeD;
-	int extremidadeE;
-	int rodadas;	
+	int qualJogador;			//variavel que guarda a vez do jogador que esta jogando
+	int extremidadeD;			//variavel que guarda a extremidade diretira da mesa
+	int extremidadeE;			//variavel que guarda a extremidade direita da mesa
+	int rodadas;				//variavel que guarda o numero de rodadas ja realizadas no jogo
 
 } Variaveis;
 
+
+	FILE *ap;					//arquivo para salvar a estrutra pecas
+	FILE *am;					//arquivo para salvar a estrutura mesa
+	FILE *av;					//arquivo para salvar a estrutura variaveis
 
 pecas Tpecas[28];
 mesa pecaMesa[28];
